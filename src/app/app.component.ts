3840a9ecefbd46673d1c497a40cc0d9ab7143869
Naderab <abdellaouinader@gmail.com>
2023-11-10 15:24:13 +0100

    seance 2 : manipulation des composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '4infini3';
+  title: string = 'Bonjour 4 INIFINI 3';
+  product = {
+    id: 1,
+    name: 'iphone',
+  };
+
+  products = [
+    {
+      id: 1,
+      name: 'iphone',
+      quantity:10
+    },
+    {
+      id: 2,
+      name: 'TV',
+      quantity:0
+    },
+  ];
+
+  getColorClass(p:any) {
+    if (p.quantity == 0) {
+      return 'red';
+    }
+    else {
+     return 'green';
+    }
+  }
+
+  Acheter(p:any) {
+    p.quantity--;
+  }
 }
